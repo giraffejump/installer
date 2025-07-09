6d110d0b83e902c4b62958442f58e4a0476632bb
GiraffeSecurity <info@giraffe.ge>
2025-07-09 21:46:59 +0400

    Update build.sh

--- utils/build.sh
+++ utils/build.sh
@@ -10,15 +10,15 @@ shopt -s expand_aliases
 # 打包
 cd "${project_dir}" || exit 3
 rm -rf "${release_dir:?}"/*
-to_dir="${release_dir}/jumpserver-installer"
+to_dir="${release_dir}/giraffejump-installer"
 mkdir -p "${to_dir}"
 
 if [[ -d '.git' ]]; then
   command -v git || yum -q -y install git
   git archive --format tar HEAD | tar x -C "${to_dir}"
 else
-  cp -R . /tmp/jumpserver
-  mv /tmp/jumpserver/* "${to_dir}"
+  cp -R . /tmp/giraffejump
+  mv /tmp/giraffejump/* "${to_dir}"
 fi
 
 shopt -s expand_aliases
